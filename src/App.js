5aac913ba582af3e2fafb2c5786c209db8d68803
kurodayuuki <kurodayuki07122170@gmail.com>
2022-05-28 11:06:08 +0900

    my first React Hook width useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  
+  const inicrement = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+  const reset = () => setCount(0)
+  const double = () => setCount(count * 2)
+  const divide3 = () => setCount(previousCount => previousCount % 3 == 0 ? previousCount / 3 : previousCount)
+
+
+  const inicrement2 = () => setCount(previousCount => previousCount + 1)
+  const decrement2 = () => setCount(previousCount => previousCount - 1)
+
   return (
-    <div>
-     This is a tempate React app.
-    </div>
+    <>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={inicrement}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+
+      <div>
+        <button onClick={inicrement2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={double}>x2</button>
+        <button onClick={divide3}>3の倍数の時だけ3で割る</button>
+      </div>
+    </>
   );
 }
 
